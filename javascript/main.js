db2c905afef40989ed29752e8f28637cdf3890d9
Chrystelle Poulet <chrystelle.poulet@hetic.net>
2018-03-14 01:12:29 +0100

    main features completed / categories filter added

--- javascript/main.js
+++ javascript/main.js
@@ -1,71 +1,108 @@
-var mainContainer = document.querySelector('.main-container');
-var weeklySelectionContainer = document.createElement('DIV');
-weeklySelectionContainer.classList.add('main-container__weekly-selection-container');
-mainContainer.appendChild(weeklySelectionContainer);
+// ------------------ display weekly selection movies ---------------------- //
 
+var selectionContainer = document.querySelector('.main__selection-container');
+var grid = document.querySelector('.main__selection-container__grid');
 var j = 1;
 
-for (var i = 0; i < data.films.length; i++) {
-  if (data.films[i].weekMovie) {
-    var weekMovie = document.createElement('IMG');
-    weekMovie.setAttribute('src', data.films[i].img);
-    weekMovie.classList.add(`main-container__weekly-selection-container__week-movie-${j}`);
-    weeklySelectionContainer.appendChild(weekMovie);
-    j++;
-  };
-}
+displayWeeklySelection();
 
-var parent = document.querySelector('.main-container');
-var child = document.querySelector('.main-container__weekly-selection-container')
-var weeklySelection = document.querySelectorAll('.main-container__weekly-selection-container > img');
-var title;
-var duration;
-var author;
-var description;
-
-for (var i = 0; i < weeklySelection.length; i++) {
-  weeklySelection[i].addEventListener('click', function(event) {
-    var src = this.getAttribute('src');
-    parent.removeChild(child);
-
-    var selectedMovieContainer = document.createElement('DIV');
-    selectedMovieContainer.classList.add('main-container__selected-movie-container');
-    mainContainer.appendChild(selectedMovieContainer);
-    var selectedMovie = document.createElement('IMG');
-    selectedMovieContainer.classList.add('main-container__selected-movie-container__img');
-    selectedMovieContainer.appendChild(selectedMovie);
-    selectedMovie.setAttribute('src', src);
-
-    var selectedMovieResumeContainer = document.createElement('DIV');
-    selectedMovieResumeContainer.classList.add('main-container__selected-movie-resume-container');
-    mainContainer.appendChild(selectedMovieResumeContainer);
-
-    createHTMLNode(title, 'P', selectedMovieResumeContainer, 'main-container__selected-movie-resume-container__title');
-
-    createHTMLNode(duration, 'P', selectedMovieResumeContainer, 'main-container__selected-movie-resume-container__duration');
-
-    createHTMLNode(author, 'P', selectedMovieResumeContainer, 'main-container__selected-movie-resume-container__author');
-
-    createHTMLNode(description, 'P', selectedMovieResumeContainer, 'main-container__selected-movie-resume-container__description');
-
-    title = document.querySelector('.main-container__selected-movie-resume-container__title');
-    duration = document.querySelector('.main-container__selected-movie-resume-container__duration');
-    author = document.querySelector('.main-container__selected-movie-resume-container__author');
-    description = document.querySelector('.main-container__selected-movie-resume-container__description');
-
-    for (var i = 0; i < data.films.length; i++) {
-      if (data.films[i].img === src) {
-        title.innerHTML = data.films[i].title;
-        duration.innerHTML = data.films[i].duration;
-        author.innerHTML = data.films[i].author;
-        description.innerHTML = data.films[i].description;
-      }
-    }
+function displayWeeklySelection() {
+  data.films.forEach(function(movie) {
+    if (movie.weekMovie) {
+      var weekMovie = document.createElement('IMG');
+      weekMovie.setAttribute('src', movie.img);
+      weekMovie.classList.add('main__selection-container__grid__week-movie');
+      grid.appendChild(weekMovie);
+      j++;
+    };
   })
 }
 
-function createHTMLNode(child, tag, parent, className) {
-  var child = document.createElement(tag);
-  child.classList.add(className);
-  parent.appendChild(child);
+// ------------------ display selected movie ---------------------- //
+
+var selection = document.querySelectorAll('.main__selection-container__grid > img');
+var selectedMovie = document.querySelector('.main__selected-movie');
+var categoryTitle = document.querySelector('.main__selection-container__categoryTitle');
+var movieTitle = document.querySelector('.main__selected-movie__movieTitle');
+var duration = document.querySelector('.main__selected-movie__description-ctn__duration');
+var author = document.querySelector('.main__selected-movie__description-ctn__author');
+var resume = document.querySelector('.main__selected-movie__description-ctn__resume');
+var backButton = document.querySelector('.main__selected-movie__button');
+var video = document.querySelector('.main__selected-movie__video');
+
+function displaySelection() {
+  selection.forEach(function(movie) {
+    movie.addEventListener('click', function(event) {
+      var src = this.getAttribute('src');
+      data.films.forEach(function(movie) {
+        if (movie.img === src) {
+          video.setAttribute('src', `videos/${movie.src}`);
+          movieTitle.innerHTML = movie.title;
+          duration.innerHTML = `Duration : ${movie.duration}`;
+          author.innerHTML = `Author : ${movie.author}`;
+          resume.innerHTML = movie.description;
+        }
+      });
+      selectionContainer.classList.add('--hidden');
+      selectedMovie.classList.add('--visible');
+    })
+  })
 }
+
+displaySelection();
+
+backButton.addEventListener('click', function() {
+  selectionContainer.classList.remove('--hidden');
+  selectedMovie.classList.remove('--visible');
+  video.pause();
+})
+
+var mobileMenu = document.querySelector('.header__menu-mobile');
+var mobileMenuDetails = document.querySelector('.header__menu-mobile-details');
+
+mobileMenu.addEventListener('click', function(event) {
+  mobileMenuDetails.classList.toggle('--visible');
+  mobileMenu.classList.toggle('--open');
+});
+
+//-------------------CATEGORIES----------------------//
+
+const categories = document.querySelectorAll('.header__menu-mobile-details__category');
+var current;
+
+categories.forEach(function(category) {
+  category.addEventListener('click', function(event) {
+    var movies = '';
+    category = event.target.textContent;
+    current = category;
+    if (category === 'Weekly selection') {
+      data.films.forEach(function(movie) {
+        if (movie.weekMovie) {
+          movies += "<img class=main__selection-container__grid__week-movie src=" + movie.img + ">";
+        }
+      })
+      categoryTitle.innerHTML = 'Weekly selection';
+    } else if (category === 'All movies') {
+      data.films.forEach(function(movie) {
+        movies += "<img class=main__selection-container__grid__week-movie src=" + movie.img + ">";
+      })
+      categoryTitle.innerHTML = 'All movies';
+    } else {
+      data.films.forEach(function(movie) {
+        if (movie.category === category) {
+          movies += "<img class=main__selection-container__grid__week-movie src=" + movie.img + ">";
+        };
+      })
+      categoryTitle.innerHTML = category;
+    }
+    if (selectionContainer.classList.contains('--hidden')) {
+      selectionContainer.classList.remove('--hidden');
+    }
+    grid.innerHTML = movies;
+    mobileMenuDetails.classList.remove('--visible');
+    mobileMenu.classList.remove('--open');
+    selectedMovie.classList.remove('--visible');
+    selection = document.querySelectorAll('.main__selection-container__grid > img');
+    displaySelection();
+  })
+})
